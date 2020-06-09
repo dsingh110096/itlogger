e4b6b7dd0748c177bdbb0b3b2b0fbf509ce3d7af
Dheerendra Singh <dsingh110096@gmai.com>
2020-06-09 16:58:14 +0530

    SearchBar and Logs component created

---
+++ src/components/layout/SearchBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const SearchBar = () => {
+  return (
+    <nav style={{ marginBottom: '30px' }} className='orange'>
+      <div className='nav-wrapper'>
+        <form autoComplete='off'>
+          <div className='input-field'>
+            <input id='search' type='search' style={{ cursor: 'pointer' }} />
+            <label className='label-icon' htmlFor='search' style={{ cursor: 'pointer' }}>
+              <i className='material-icons'>search</i>
+            </label>
+            <i className='material-icons'>close</i>
+          </div>
+        </form>
+      </div>
+    </nav>
+  );
+};
+
+export default SearchBar;
---
+++ src/components/logs/Logs.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+
+const Logs = () => {
+  const [logs, setLogs] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getLogs();
+    //eslinst-disable-next-line
+  }, []);
+
+  const getLogs = async () => {
+    setLoading(true);
+    const res = await fetch('/logs');
+    const data = await res.json();
+
+    setLogs(data);
+    setLoading(false);
+  };
+  if (loading) {
+    return <h4>Loading....</h4>;
+  }
+  return (
+    <ul className='collection with-header'>
+      <li className='collection-header'>
+        <h4 className='center'>System Logs</h4>
+      </li>
+      {!loading && logs.length === 0 ? (
+        <p className='center'>No logs to Show...</p>
+      ) : (
+        logs.map((log) => (
+          <li className='collection-item' key={log.id}>
+            {log.message}
+          </li>
+        ))
+      )}
+    </ul>
+  );
+};
+
+export default Logs;
